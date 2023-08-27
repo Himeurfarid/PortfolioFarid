97a9edcf666c01efc77aebf2ff6d6f01bc7941bd
Farid <faridhimeur@outlook.fr>
2023-08-27 17:02:59 +0200

    installer prettier eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,24 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-
 import "./index.css";
 import App from "./App";
+import { Router, Route, Routes } from "@solidjs/router";
 
 const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
-render(() => <App />, root);
+render(
+  () => (
+    <Router>
+      <Routes>
+        <Route path="/" component={App} /> {/* 👈 Define the home page route */}
+      </Routes>
+    </Router>
+  ),
+  document.getElementById("root")
+);
